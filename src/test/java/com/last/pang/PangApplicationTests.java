5cada94f6029c6f86c417819791e393a83b03592
thekim123 <tacdo12@naver.com>
2023-07-26 21:12:52 +0900

    initial commit

---
+++ src/test/java/com/last/pang/PangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.last.pang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
